319ab2d41470f805e9e60088b606f2e499df33fa
rownygosc1 <137772256+rownygosc1@users.noreply.github.com>
2024-04-18 14:07:39 +0200

    Add files via upload

---
+++ biuro.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki krajoznawcze</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+<body>
+    <div class="baner">
+        <h1>Witamy w biurze podróży</h1>
+    </div>
+    <div class="dane">
+        <h3>Archiwum wycieczek</h3>
+        <table>
+            <tr>
+            <td>5.Francja, Paryz, cena:1350 </td>
+            </tr>
+            <tr>
+            <td>7.Hiszpania, Barcelona, cena:1500</td>
+            </tr>
+            <tr>
+            <td>8.Hiszpania, Barcelona, cena:1500</td>
+            </tr>
+            </table>
+        <?php
+
+        ?>
+    </div>
+    <div class="lewy">
+        <h3>Najtaniej...</h3>
+        <table>
+            <tr>
+                <td>Włochy</td>
+                <td>od 1200zł</td>
+            </tr>
+            <tr>
+                <td>Francja</td>
+                <td>od 1200zł</td>
+            </tr>
+            <tr>
+                <td>Hiszpania</td>
+                <td>od 1400zł</td>
+            </tr>
+        </table>
+    </div>
+    <div class="srodkowy">
+        <div class="row">
+        <h3>Tu byliśmy</h3>
+        </div>
+        
+        <br>
+        <div class="row">
+            <div class="image"><img src="1.jpg" alt="Obrazek 1"></div>
+            <div class="image"><img src="2.jpg" alt="Obrazek 2"></div>
+            <div class="image"><img src="3.jpg" alt="Obrazek 3"></div>
+        </div>
+        <div class="row">
+            <div class="image"><img src="4.jpg" alt="Obrazek 4"></div>
+            <div class="image"><img src="5.jpg" alt="Obrazek 5"></div>
+            <div class="image"><img src="6.jpg" alt="Obrazek 6"></div>
+        </div>
+        <div class="row">
+            <div class="image"><img src="7.jpg" alt="Obrazek 4"></div>
+            <div class="image"><img src="8.jpg" alt="Obrazek 5"></div>
+            <div class="image"><img src="9.jpg" alt="Obrazek 6"></div>
+        </div>
+        <!-- skrypt2 -->
+    </div>
+    <div class="prawy">
+        <h3>Skontaktuj się</h3>
+        <a href="wycieczki@wycieczki.pl">napisz do nas</a>
+        <p>telefon:555666777</p>
+    </div>
+    <div class="stopka">
+        <p>Stronę wykonał: numer pesel</p>
+    </div>
+
+    
+</body>
+</html>
